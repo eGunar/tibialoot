a8c35c1cacea860c7086ed8b74547016f0d6a709
eGunar <egunnar1999@gmail.com>
2017-04-06 15:33:07 +0200

    update db

--- install.py
+++ install.py
@@ -1,7 +1,6 @@
 from models import *
 
 
-
 db.connect()
 db.create_tables([Rashid, Nahbob, Haroun, Alesar, Yaman, Yasir, Lailene, Telas, Tesha, Tamoril, Alexander, Esrik, Bone_master, Player_items, Gold])
 
